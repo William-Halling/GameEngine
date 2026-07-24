4d46b589573a62e94569f8191b1c38d97b540a85
Jinxed Nation William Halling <32233703@student.murdoch.edu.au>
2026-07-24 21:14:40 +0800

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "Engine.h"
+
+int main(int argc, char** argv)
+{
+    try
+    {
+        Engine engine;
+        engine.Run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Fatal: " << e.what() << '\n';
+        return 1;
+    }
+
+    return 0;
+}
